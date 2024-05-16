9867b6ccb549040c840e7af90552c12e06232b3a
travisahumphreys <138328517+travisahumphreys@users.noreply.github.com>
2024-05-16 21:24:47 +0000

    functioning file-read

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv
+import csv
 # import svgutils
 
-with open('/csv/test.csv', 'r') as file:
-    reader = csv.reader(file)
+with open('./csv/test.csv', 'r') as file:
+    reader = csv.reader(file, delimiter=',')
     for row in reader:
-        print(row) 
+        print(row)
